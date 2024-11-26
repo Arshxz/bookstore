8b6948ba1e28faf2ce0118537486b6f5d25d76e5
Arsh <arsh@Arshs-MacBook-Air.local>
2024-11-26 15:29:29 -0500

    abc

--- Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
+++ Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eabbd0de8ad3d700424d35973ca1dfa0f50c142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe096f281379c4846bdf8b2983d6ba95eb50cd07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
